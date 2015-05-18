fdc6915db39a7570fa5df3480bc2c7de387d951f
Simon Krause <simon.krause@kekkimekki.de>
2015-05-18 11:03:19 +0200

    Datentypen angepaßt und einige Funktionen hinzugefügt

--- list.h
+++ list.h
@@ -15,7 +15,7 @@
 typedef struct ListElem {
 	ListElem* prev;
 	ListElem* next;
-	uint16_t blockNr;
+	uint32_t blockNr;
 } ListElem_t;
 
 /*	Datenstruktur für die nach Anzahl der Löschvorgänge sortiere doppel verkettete Liste
@@ -28,9 +28,9 @@ typedef struct ListElem {
 typedef struct {
 	ListElem_t* first;
 	ListElem_t* last;
-	uint16_t AVG;
-	uint16_t blockCounter;
-	Block_t** blockArray;
+	uint32_t AVG;
+	uint32_t blockCounter;
+	Block_t* blockArray;
 } List_t;
 
 // PUBLIC Funktionen
@@ -39,7 +39,7 @@ typedef struct {
  *	Initialisiere eine neue leere Liste und gebe diese zurück
  *	Übergabeparameter ist ein Pointer auf das Blockarray des ftl
  */
-List_t* initList(Block_t** blockArray);
+List_t* initList(Block_t* blockArray);
 
 /*
  *	Gebe allokierten Speicher dieser Liste zurück und gebe diese frei
@@ -54,11 +54,22 @@ void addBlock(List_t* list, uint16_t blockNr);
 /*
  *	Gebe den ersten Block(BlockNr) dieser Liste zurück
  */
-uint16_t getFirstBlock(List_t* list);
+uint32_t getFirstBlock(List_t* list);
 
 /*
  *	Gebe den letzten Block(BlockNr) dieser Liste zurück
  */ 
-uint16_t getLastBlock(List_t* list);
+uint32_t getLastBlock(List_t* list);
+
+/*
+ *	Berechne AVG dieser List nach einem neuen Löschvorgang neu
+ */
+void recalculationAVG(List_t* list);
+
+/*
+ *	Überprüft, ob gegebene Blocknummer in dieser List enthalten ist
+ *	und gibt TRUE, wenn enthalten und FALSE, wenn nicht
+ */
+bool isElementOfList(List_t* list, uint32_t blockNr);
 
 #endif
